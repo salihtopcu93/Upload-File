f30c72e717860aa10fb45df4b8cc549ab90489a3
Salih Topçu <mba@MBAs-MacBook-Air.local>
2017-11-09 11:00:56 +0300

    Initial Commit

---
+++ forgotPassword/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  forgotPassword
+//
+//  Created by raSe on 9.11.2017.
+//  Copyright © 2017 raSe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
